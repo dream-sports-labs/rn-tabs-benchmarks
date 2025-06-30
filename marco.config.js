3b557e19a4f54c74cbeb0221f5361520b57b0479
Shubham <85783070+shubhamguptadream11@users.noreply.github.com>
2025-06-30 12:50:33 +0530

    feat: marco version upgrade (#7)
    * feat: marco version upgrade
    
    * feat: redundant files removed
    
    * feat: added support for github pages

--- marco.config.js
+++ marco.config.js
@@ -2,7 +2,57 @@ module.exports = {
     android: {
       packageName: 'com.rntabsbenchmarks',
       outputPath: './reports/android/oneplus/',
-      dataDir: 'reports/android/oneplus/log.json',
+      dataDir: [
+        {
+          path: 'reports/android/oneplus/js-tab-load.json',
+          reportName: 'Android HighEnd JS Tab Load',
+        },
+        {
+          path: 'reports/android/oneplus/native-tab-load.json',
+          reportName: 'Android HighEnd Native Tab Load',
+        },
+        {
+          path: 'reports/android/oneplus/js-tab-switch.json',
+          reportName: 'Android HighEnd JS Tab Switch',
+        },
+        {
+          path: 'reports/android/oneplus/native-tab-switch.json',
+          reportName: 'Android HighEnd Native Tab Switch',
+        },
+        {
+          path: 'reports/android/vivo/js-tab-load.json',
+          reportName: 'Android LowEnd JS Tab Load',
+        },
+        {
+          path: 'reports/android/vivo/native-tab-load.json',
+          reportName: 'Android LowEnd Native Tab Load',
+        },
+        {
+          path: 'reports/android/vivo/js-tab-switch.json',
+          reportName: 'Android LowEnd JS Tab Switch',
+        },
+        {
+          path: 'reports/android/vivo/native-tab-switch.json',
+          reportName: 'Android LowEnd Native Tab Switch',
+        },
+        {
+          path: 'reports/ios/js-tab-load.json',
+          reportName: 'iOS JS Tab Load',
+        },
+        {
+          path: 'reports/ios/native-tab-load.json',
+          reportName: 'iOS Native Tab Load',
+        },
+        
+        {
+          path: 'reports/ios/js-tab-switch.json',
+          reportName: 'iOS JS Tab Switch',
+        },
+        {
+          path: 'reports/ios/native-tab-switch.json',
+          reportName: 'iOS Native Tab Switch',
+        },
+      ],
       port: '8080',
     },
     ios: {
